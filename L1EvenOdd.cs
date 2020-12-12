3ce3435f82053e09eaa33175e7acbb204c5dd984
shubham-8496 <shubham.sonawane8496@gmail.com>
2020-12-12 18:14:05 +0530

    Lambda assignment

---
+++ L1EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdaAssignments
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Predicate<int> evnOdd = a => a % 2 == 0 ? true : false;
+
+            Console.WriteLine(evnOdd(n1));
+            Console.ReadLine();
+        }
+
+    }
+}
+
