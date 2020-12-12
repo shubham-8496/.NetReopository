31f45c06afffded98d10f8b121a1a8705c7a37cc
shubham-8496 <shubham.sonawane8496@gmail.com>
2020-12-12 18:15:22 +0530

    Lambda assignment

---
+++ L4LambdaSI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Principal: ");
+            decimal p = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter year period: ");
+            decimal r = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter Rate of interest in %: ");
+            decimal n = Convert.ToDecimal(Console.ReadLine());
+
+            Func<decimal, decimal, decimal, decimal> Sterest = (a, b, c) => ((a * b * c) / 100);
+            Console.WriteLine("Simple Interset is: " + Sterest(p, r, n));
+
+            Console.ReadLine();
+        }
+    }
+}
